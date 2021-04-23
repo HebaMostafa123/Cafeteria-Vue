4f2cbfe1a9740b4c6057c4b3d61e053647857985
HebaMostafa123 <hoba.mostafa97@gmail.com>
2021-04-24 01:59:27 +0200

    feature(list-proccessed-order):add deliver feature

--- src/apis/Order.js
+++ src/apis/Order.js
@@ -1,8 +1,10 @@
 import Api from "./Api";
-
 export default {
   //admin page list proccessed orders
   async getProcessingOrders() {
     return Api.get("/orders");
+  },
+  async changeStatus(id, router) {
+    Api.patch(`/orders/${id}`);
   },
 };
--- src/components/public/listOrder/proccessedOrders.vue
+++ src/components/public/listOrder/proccessedOrders.vue
@@ -20,24 +20,30 @@
             <td>{{ order.room.number }}</td>
             <td>{{ order.room.ext }}</td>
             <td>
-              <button type="submit" class="btn btn-primary btn-lg">
+              <button
+                type="submit"
+                class="btn btn-primary btn-lg"
+                @click="changeStatus(order)"
+              >
                 Deliver
               </button>
             </td>
           </tr>
         </tbody>
       </table>
-      <div v-for="product of order.products">
-        <img class="product_image d-flex flex-row" :src="product.image" />
-        <div class="product-info">
-          <div class="product-price-btn">
-            <p align="center">
-              <span>{{ product.price }}</span
-              >EGP
-            </p>
+      <div class="d-flex flex-row justify-content-around">
+        <div class="product" v-for="product of order.products">
+          <img class="product_image" :src="product.image" />
+          <div class="product-info">
+            <div class="product-price-btn">
+              <p align="center">
+                <span>{{ product.price }}</span> EGP
+                <span> Quantity:{{ product.pivot.quantity }}</span>
+              </p>
+              <!-- <span> Quantity:{{ product.pivot.quantity }}</span> -->
+            </div>
           </div>
         </div>
-        <span> Quantity:{{ product.pivot.quantity }}</span>
       </div>
     </div>
   </div>
@@ -45,7 +51,7 @@
 
 <script>
 import Orders from "../../../apis/Order";
-
+// import Router from "vue-router";
 export default {
   /* Component's local data */
   data() {
@@ -54,15 +60,18 @@ export default {
     };
   },
   methods: {
-    // formatDate(date) {
-    //   console.log(date);
-    //   return moment(date).format("dd.mm.YYYY");
-    // },
+    changeStatus(order) {
+      Orders.changeStatus(order.id, this.$router);
+      //   console.log(order.id);
+      Orders.getProcessingOrders().then((response) => {
+        this.orders = response.data.data;
+      });
+    },
   },
   computed: {},
   mounted() {
     Orders.getProcessingOrders().then((response) => {
-      console.log(response.data.data);
+      //   console.log(response.data.data);
       this.orders = response.data.data;
     });
   },
@@ -80,7 +89,7 @@ export default {
   padding-bottom: 0px;
 }
 .product-info {
-  float: left;
+  /* float: left; */
   height: 1.5rem;
   width: 7rem;
   border-radius: 7px;
@@ -91,12 +100,13 @@ export default {
   height: 2rem;
   width: 100%;
   position: relative;
+  align-content: center;
 }
 .product-price-btn p {
   display: inline-block;
   position: absolute;
   height: 7vh;
-  width: 4rem;
+  width: 100%;
   font-size: 0.9rem;
   font-weight: lighter;
   color: #474747;
@@ -105,5 +115,11 @@ span {
   display: inline-block;
   height: 7vh;
   font-size: 1.2rem;
+}
+.product {
+  width: 200px;
+  height: 200px;
+  margin: 0px;
+  /* text-align: center; */
 }
 </style>
